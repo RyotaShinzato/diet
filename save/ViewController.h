6e3e8e6a03350a02aa5e1de28d2248512cab264a
snzt <e115702@ararinyan.local>
2013-10-28 21:05:10 +0900

    can input and save

--- save/ViewController.h
+++ save/ViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *input_weight;
+@property (weak, nonatomic) IBOutlet UITextField *input_period;
+@property (weak, nonatomic) IBOutlet UIButton *save;
 
+- (IBAction)save:(id)sender;
 @end
--- save/ViewController.m
+++ save/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,4 +27,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)save:(id)sender {
+    NSUserDefaults *weight = [NSUserDefaults standardUserDefaults];
+    NSUserDefaults *period = [NSUserDefaults standardUserDefaults];
+    [weight setObject:self.input_weight.text forKey:@"my_weight"];
+    [weight setObject:self.input_period.text forKey:@"my_period"];
+    int w = [weight integerForKey:@"my_weight"];
+    int p = [period integerForKey:@"my_period"];
+    NSLog(@"体重は%d",w);
+    NSLog(@"期間は%d",p);
+    
+}
 @end
